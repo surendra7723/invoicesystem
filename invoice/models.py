80ac6339aa6abaf3bb9b3116fdb6396e143df8c0
Surendra Thapa <surendra.thapa@ayulogic.com>
2025-03-25 08:15:57 +0545

    added company models and added default authentication for api

--- invoice/models.py
+++ invoice/models.py
@@ -3,24 +3,45 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 from django.core.validators import MinValueValidator
 
+class Company(models.Model):
+    CATEGORY_TECH = 'TECH'
+    CATEGORY_FINANCE = 'FIN'
+    CATEGORY_HEALTH = 'HEALTH'
+    CATEGORY_EDUCATION = 'EDU'
+
+    COMPANY_CATEGORIES = [
+        (CATEGORY_TECH, 'Technology'),
+        (CATEGORY_FINANCE, 'Finance'),
+        (CATEGORY_HEALTH, 'Health'),
+        (CATEGORY_EDUCATION, 'Education'),
+    ]
+    size = models.CharField(max_length=50)
+    total_employee = models.PositiveIntegerField()
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    category = models.CharField(max_length=50,choices=COMPANY_CATEGORIES)
+    
+
 class Customer(AbstractUser):
-    full_name=models.CharField(max_length=100)
-    email=models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
     
     
     def __str__(self):
         return self.full_name
     
     
-    
-    
-
-# Create your models here.
 class Invoice(models.Model):
-    
-    date_created=models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)    
+    date_created = models.DateTimeField(auto_now_add=True)
     # total_amount=models.DecimalField(max_digits=10,decimal_places=2,validators=[MinValueValidator(0.0)],default=0)
-    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE, default=1)  # Assuming '1' is a valid Company ID
     
     def __str__(self):
         return f"Invoice of {self.customer.full_name} having ID:{self.id}"
@@ -43,9 +64,9 @@ class InvoiceItem(models.Model):
         return self.quantity*self.unit_price
     def __str__(self):
         return f"{self.quantity} - {self.unit_price}"
-    
-    
-    
-    
-    
-  
+
+
+
+
+
+
--- invoiceproject/settings.py
+++ invoiceproject/settings.py
@@ -128,6 +128,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK={
     "DEFAULT_PERMISSION_CLASSES":[
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
-}
+    }
 AUTH_USER_MODEL = "invoice.Customer"
