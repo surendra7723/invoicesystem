1158d29a2cfacb037371723c237db1734e30908e
Surendra Thapa <surendrathapa986@gmail.com>
2025-03-28 08:28:53 +0545

    setup with postgreSql

--- invoice/tests.py
+++ invoice/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Company
 
-# Create your tests here.
+class CompanyModelTest(TestCase):
+    def setUp(self):
+        Company.objects.create(name="Test Company", category="TECH", email="test@example.com")
+
+    def test_company_creation(self):
+        company = Company.objects.get(name="Test Company")
+        self.assertEqual(company.category, "TECH")
+        self.assertEqual(company.email, "test@example.com")
--- invoiceproject/settings.py
+++ invoiceproject/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'invoiceproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invoicedb',
+        'USER': 'surendra',
+        'PASSWORD': 'surendra123',
+        'HOST': 'localhost',  # Or the IP address of your PostgreSQL server
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
